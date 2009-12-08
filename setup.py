4f408acab9e0a26323e3185b29d617e738f5493c
Rick Copeland <rick446@legolas-vm.(none)>
2009-12-08 11:07:28 -0500

    Update package metadata

--- setup.py
+++ setup.py
@@ -6,14 +6,22 @@ version = '0.1'
 setup(name='Ming',
       version=version,
       description="Bringing order to Mongo since 2009",
-      long_description="""\
+      long_description="""Database mapping layer for MongoDB on Python. Includes schema enforcement and some facilities for schema migration. 
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='mongo, pymongo',
       author='Rick Copeland',
       author_email='rick@geek.net',
-      url='',
-      license='Apache',
+      url='http://merciless.sourceforge.net',
+      license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=True,
