f9e67da3fdf8483ac2ba15cae8cce47d7ad5adb8
Dillon Walls <dillon.walls@slashdotmedia.com>
2024-06-20 16:07:26 -0400

    maybe fixed nearly 10 year old issue with ming's replace_one

--- ming/mim.py
+++ ming/mim.py
@@ -489,7 +489,7 @@ class Collection(collection.Collection):
             return _id
 
     def replace_one(self, filter, replacement, upsert=False):
-        return self._update(filter, replacement, upsert)
+        return self.__update(filter, replacement, upsert)
 
     def __update(self, spec, updates, upsert=False, multi=False):
         bson_safe(spec)
